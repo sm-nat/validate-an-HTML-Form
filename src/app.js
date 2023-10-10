47e8444b69ede6a00e408a1428ae105675de1359
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-10-10 21:16:05 +0000

    starting the validation

--- src/app.js
+++ src/app.js
@@ -2,9 +2,57 @@
 import "bootstrap";
 import "./style.css";
 import "./assets/img/4geeks.ico";
+import { left } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
 
-  console.log("Hello Rigo from the console!");
+  let regexCardNumber = /^[0-9]{16}$/;
+  let regexCVC = /^[0-9]{3}$/;
+  let regexAmount = /^[0-9]+(\.[0-9]{1,2})?$/;
+  let regexName = /^[A-Za-z]+$/;
+  let regexLastName = /^[A-Za-z]+$/;
+  let regexCity = /^[A-Za-z]+$/;
+  let regexPostalCode = /^[0-9]{6}$/;
+
+  document.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector(".myForm");
+    let missingFieldsAlert = document.getElementById("missingFieldsAlert");
+
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+
+      let {
+        Card,
+        Amount,
+        CVC,
+        Name,
+        LastName,
+        City,
+        State,
+        PostalCode
+      } = e.target;
+
+      let Card = document.querySelector("#usercard");
+      let Amount = document.querySelector("#useramount");
+      let CVC = document.querySelector("#usercvc");
+      let Name = document.querySelector("#username");
+      let LastName = document.querySelector("#userlastname");
+      let City = document.querySelector("#usercity");
+      let State = document.querySelector("#userstate");
+      let PostalCode = document.querySelector("#userpostalcode");
+
+      if (validateForm()) {
+        alert("Sending...");
+      } else {
+        missingFieldsAlert.style.display = "block";
+      }
+    });
+
+    function validateForm() {
+      // Agrega lógica de validación aquí
+      // Retorna true si el formulario es válido, de lo contrario, false
+      return true;
+    }
+  });
 };
